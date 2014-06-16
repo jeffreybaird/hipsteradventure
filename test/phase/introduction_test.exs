3627267efbc9bbd7af59674004a5def7eb1484b1
jeffreybaird <jeff@jeffreyleebaird.com>
2014-06-15 20:41:16 -0700

    updating tests

--- test/phase/introduction_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule Phase.IntroductionTest do
-  use ExUnit.Case
-
-  test "new with no args should return" do
-    assert 1 + 1 == 2
-  end
-end
--- test/player_test.exs
+++ test/player_test.exs
@@ -2,6 +2,7 @@ defmodule PlayerTest do
   use ExUnit.Case
 
   import Player
+
   test "new with no args should return" do
     assert is_map(new)
   end
@@ -15,6 +16,12 @@ defmodule PlayerTest do
   test "returns a players name when asked" do
     player = add_name(new, "jeff")
     assert player.name == "jeff"
+  end
+
+  test "returns the number of cigarettes when asked" do
+    player = add_name(new, "jeff")
+    player_with_cig = Player.add_cigarette(player)
+    assert Player.cigarettes(player_with_cig) == 1
   end
 
 end
